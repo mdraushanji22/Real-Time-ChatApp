18a5b50349d88973d845a3b17d6b5df488c12582
Md Raushan Jilani <90023835+mdraushanji22@users.noreply.github.com>
2025-10-29 22:41:14 +0530

    Update baseURL for production and development

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // Updated to use deployed backend URL in production
 const baseURL = import.meta.env.MODE === "development" 
-  ? "http://localhost:4000/api/v1" 
-  : "https://real-time-chatapp-backend-foiz.onrender.com/api/v1";
+  ? "https://real-time-chatapp-backend-foiz.onrender.com/api/v1" 
+  : "/api/v1";
 
 export const axiosInstance = axios.create({
   baseURL,
